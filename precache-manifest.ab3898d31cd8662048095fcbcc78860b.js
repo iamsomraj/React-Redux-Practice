5f557a583ffa9999c6972eea7c844d075bc9a2ac
iamsomraj <iamsomraj@gmail.com>
2020-07-27 00:45:07 +0530

    Updates

---
+++ precache-manifest.ab3898d31cd8662048095fcbcc78860b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bfd5034748990968315d808c5767eb0",
+    "url": "/React-Redux-Practice/index.html"
+  },
+  {
+    "revision": "b1317615190128b8a0f0",
+    "url": "/React-Redux-Practice/static/js/2.34543ed0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Redux-Practice/static/js/2.34543ed0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb3ecba9c27140285520",
+    "url": "/React-Redux-Practice/static/js/main.d8544244.chunk.js"
+  },
+  {
+    "revision": "6aca62bb01daef2f4249",
+    "url": "/React-Redux-Practice/static/js/runtime-main.02020600.js"
+  }
+]);
